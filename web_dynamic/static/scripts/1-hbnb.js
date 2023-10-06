c7d4d58fa079e75f4dde5ad24d2a2ae4acb74bed
shaco-star <sherifm846@gmail.com>
2023-10-06 10:42:10 -0700

    task2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,17 @@
-$('document').ready(function () {
-    const dict = {};
-    $('input[type=checkbox]').change(function () {
-        const name = $(this).attr('data-name');
-        if ($(this).is(':checked')) {
-            dict[$(this).attr('data-id')] = name;
-        } else {
-            delete dict[$(this).attr('data-id')]
-        }
-        $('.amenities h4').text(Object.values(dict).join(', '))
-    })
+$(document).ready(function () {
+  const amenityObj = {};
+
+  $('.amenities .popover input').on('change', function () {
+    const name = $(this).attr('data-name');
+    const id = $(this).attr('data-id');
+
+    if ($(this).is(':checked')) {
+      amenityObj[name] = id;
+    } else {
+      delete amenityObj[name];
+    }
+
+    const names = Object.keys(amenityObj).sort().join(', ');
+    $('.amenities h4').text(names);
+  });
 });
